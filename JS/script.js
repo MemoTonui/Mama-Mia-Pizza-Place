02ea6100ed0407c2415d3bc46e538d9eb21548e0
Linda Tonui <tonuilinda@gmail.com>
2020-05-25 00:05:09 +0300

    Add final code

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,19 @@
 /*Business Logic*/
 var price,drinkPrice,toppingPrice ;
 var totalPrice = 0;
+var checkout = 0;
+let deliveryFee= checkout+400;
 
-    function pizza(name,size,toppings,drinks,quantity,totalPrice){
+
+
+
+    function pizza(name,size,toppings,drinks,quantity,total){
         this.name = name
         this.size = size;
         this.toppings= toppings;
         this.drinks = drinks;
         this.quantity = quantity;
-        this.totalPrice = totalPrice
+        this.total = total;
     }
 
 
@@ -19,51 +24,48 @@ $(".orderNow").click(function(event){
 event.preventDefault();
 });
     /*Get values from the user*/
-$("button#addToCart").click(function(){
+$("button#addToCart").click(function(event){
+    event.preventDefault();
     var name =$("#pizzaname option:selected").val();
-    alert(name);    
+        
     var pizzaSize = $("#pizzasize option:selected").val();
-   alert(pizzaSize);
+   
     var pizzaDrinks = $("#pizzadrinks option:selected").val();
-    alert(pizzaDrinks);
+    
     var pizzaToppings = [];
-        $.each($("input [name='toppings']:checked")),function(){
-       pizzaToppings.push($(this));
-            }
-       console.log(pizzaToppings.join(" "));
-       alert(pizzaToppings);
-    var quantity = parseInt($("#quantity").val());
-    alert(quantity);
-        if(quantity == " "){
-            alert("please enter the quantity!");
-        }
-        else{
-            console.log(quantity);
-        }
+        $.each($("input[name='toppings']:checked"),function(){
+       pizzaToppings.push($(this).val());
+            });
+       
+       var quantity = parseInt($("#quantity").val());
+       
+    while(checkout===NaN){
+        alert("Please Indicate the quantity");
+    }
+        
 
     
     
 
-                /*Pizza Size*/
+           /*Pizza Size*/
                 switch(pizzaSize){
-                    case 'small':
+                    case 'Small':
                         price = 600;
                         break;
                 
-                    case 'medium':
+                    case 'Medium':
                         price = 800;
                         break;
-                    case 'large':
+                    case 'Large':
                         price = 1000;
                         break;
-                    case 'xLarge':
+                    case 'Extra Large':
                         price = 1200;
                         break;
-                    case 'sdLarge' :
+                    case 'Super Duper Large' :
                         price = 1500;  
                         break; 
                 }
-                alert(price);
 
                 console.log(pizzaSize);
 
@@ -74,8 +76,7 @@ $("button#addToCart").click(function(){
                 else{
                     drinkPrice= 80;
                 }
-                alert(drinkPrice)
-                console.log(pizzaDrinks)
+               
 
                 /*Pizza toppings*/
                 toppingPrice = pizzaToppings.length*150;
@@ -83,13 +84,11 @@ $("button#addToCart").click(function(){
                 /*Total Price*/
                 totalPrice =(price + toppingPrice)*quantity;
                 console.log(totalPrice);
-                alert(totalPrice);
-
-                var checkout = 0;
+                
                 checkout = totalPrice+drinkPrice;
-                alert(checkout)
-                console.log(checkout);
+                
 
+               
                 $("#pizzaName").html($("#pizzaname option:selected").val());
                 $("#pizzaSize").html( $("#pizzasize option:selected").val());
                 $("#pizzaDrink").html($("#pizzadrinks option:selected").val());
@@ -98,72 +97,83 @@ $("button#addToCart").click(function(){
                 $("#totals").html(checkout);
 
                 /*Appends the information to the table*/
-                var makeOrder = new pizza(name,pizzaSize,pizzaToppings,pizzaDrinks,quantity,totalPrice);
-                $("#viewOrders").append('<tr><td id="pizzaName"'+makeOrder.name+'</td><td id="pizzaSize">' + makeOrder.size + '</td><td id="pizzaTopping">'+makeOrder.toppings + '</td><td id="pizzaDrink">'+makeOrder.drinks+ '<td id=" pizzaQuantity"'+makeOrder.quantity+'</td><td id="totals">'+makeOrder.totalPrice+'</td></tr>');
-                alert(makeOrder.name);
-                alert(makeOrder.size);
-                alert(makeOrder.toppings);
-                console.log(makeOrder.name);
+                var makeOrder = new pizza(name,pizzaSize,pizzaToppings,pizzaDrinks,quantity,checkout);
+                $(".choice").fadeIn(2000);
+                $("table#viewOrders").append('<tr><td id="pizzaName"'+makeOrder.name+'</td><td id="pizzaSize">' + makeOrder.size + '</td><td id="pizzaTopping">'+makeOrder.toppings + '</td><td id="pizzaDrink">'+makeOrder.drinks+ '<td id=" pizzaQuantity"'+makeOrder.quantity+'</td><td id="totals">'+makeOrder.total+'</td></tr>');
+                
             });
-
+    
                 
                 /*Total Food Bill*/
                 
-                $("button#checkout").click(function(){ 
-                    alert("work");
-                    $("button#checkout").hide();
-                    $("button.deliver").slideDown(1000);
-                    $("#addedprice").slideDown(1000);
+                $("button#checkout").click(function(event){
+                    event.preventDefault(); 
+                    $("div.choose").fadeIn(2000);
+                    $("button#checkout").fadeOut(1000);
+                    $("#addedprice").slideDown(2000);
                     console.log("Your total bills is sh. "+checkout);
-                    $("#pizzatotal").append("Your bill is sh. "+checkout);
+                    $("#pizzatotal").text("Your bill is sh. "+checkout);
+                    
                 });
 
                 /*Total fee + Delivery*/
                 $("button.deliver").click(function(event){
                     event.preventDefault();
-                    alert("work");
-                    $(".pizzatable").hide();
-                    $(".choice h2").hide();
-                    $(".delivery").slideDown(1000);
+                    $(".delivery").fadeIn(2000);
+                    $(".pizzatable").fadeOut(1000);
+                    $(".choice h4").fadeOut(1000);
+                    $(".choose").hide();
                     $("#addedprice").hide();
                     $("button.deliver").hide();
                     $("#pizzatotal").hide();
-                    let deliveryFee= checkoutTotal+400;
+                    
                     console.log("You will pay sh. "+deliveryFee+" on delivery");
                     $("#totalbill").append("Your bill plus delivery fee is: "+deliveryFee);
+                    
 
-                    /*Delivery Information*/
+                   
+                });
+
+                    /* Pick Up*/
+                    $("button.pick").click(function(event){
+                        event.preventDefault();
+                        $(this).hide();
+                        $(".pizzatable").hide();
+                        $(".choice h4").hide();
+                        $(".delivery").hide();
+                        $("#addedprice").hide();
+                        $("button.deliver").hide();
+                        $("#pizzatotal").hide();
+                        var idNo = Math.random(0,10);
+                        console.log("Your Food costs: sh."+checkout+"Your id is : "+idNo);
+                        $("totalbill").fadeIn(2000);
+                    $("#totalbill").text("Your total bill is sh. "+checkout+". Your food will be ready in less than an hour.Please pick it in time. Your number is : " +idNo+" .Please present your number to receive your food.Thank You!");
+
+                    });
+                    $("#done").click(function(event){
+                        event.preventDefault();
+
+                         /*Delivery Information*/
 
                     let person = $("input#name").val();
                     let phone = $("input#phone").val();
                     let location = $("input#location").val();
 
-                    if ($("input#name").val() && $("input#phone").val() && $("input#location").val()!=""){
-
-                    $("#finallmessage").append(person+", We have recieved your order and it will be delivered to you at "+location+ ". Prepare sh. "+deliveryFee);
-                    $("#totalbill").hide();
-                    $("#finallmessage").slideDown(1200);
-                    }
-                    else {
-                    alert("Please fill in the details for delivery!");
+                    if (person ==""||phone == ""||location== ""){
+                        alert("Please fill in the details for delivery!");
                     $(".delivery").show();
                     $("button#final-order").show();
                     }
-                });
+                       
+                    else {
 
-                    /* Pick Up*/
-                    $("button.pick").click(function(){
-                        $(".pizzatable").hide();
-                        $(".choise h2").hide();
-                        $(".delivery").slideDown(1000);
-                        $("#addedprice").hide();
-                        $("button.deliver").hide();
-                        $("#pizzatotal").hide();
-                        var idNo = Math.random(0,100);
-                        console.log("Your Food costs: sh."+checkoutTotal+"Your id is : "+idNo);
-                    $("#totalbill").append("Your total bill is "+checkoutTotal+".Your food will be ready in less than an hour.Please pick it in time.You number is :" +idNo+"Please present your number to receive your food.");
-
-                    });
+                        $(".delivery").fadeOut(1500)
+                        $("#finalMessage").fadeIn(1500);
+                        var finalMessage ="Thank You "+person+". We have recieved your order and it will be delivered to you at "+location+ " within an hour. Prepare sh. "+deliveryFee+ " which we will take on delivery.";
+                        $("#finalMessage").append(finalMessage);
+                        
+                        }
+                    })
                    
                 });
 
